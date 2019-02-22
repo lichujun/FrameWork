8d88d06c5328bd692209759e3b99ec90fbdfda99
李楚军 <lcj0823>
2019-02-22 10:18:56 +0800

    event loop代码优化

--- http-server/src/main/java/com/lee/http/server/vertx/verticle/EventLoopVerticle.java
+++ http-server/src/main/java/com/lee/http/server/vertx/verticle/EventLoopVerticle.java
@@ -2,6 +2,7 @@ package com.lee.http.server.vertx.verticle;
 
 import com.lee.http.bean.ControllerInfo;
 import com.lee.http.bean.PathInfo;
+import com.lee.http.bean.RequestMethod;
 import com.lee.http.core.ScanController;
 import com.lee.http.server.vertx.VertxWebServer;
 import com.lee.http.server.vertx.codec.HttpRequest;
@@ -11,10 +12,12 @@ import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 import org.apache.commons.collections4.MapUtils;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * event loop
@@ -48,63 +51,22 @@ public class EventLoopVerticle extends AbstractVerticle {
      * 路由GET请求
      */
     public void routeGetReq(PathInfo pathInfo, ControllerInfo controllerInfo) {
-        EventBus eb = vertx.eventBus();
-        // event bus传递消息的路径
-        String path = pathInfo.getHttpMethod() + pathInfo.getHttpPath();
         router.get(pathInfo.getHttpPath())
-                .handler(rc -> {
-                    Map<String, Class<?>> paramMap = controllerInfo.getMethodParameter();
-                    // 入参为空，则无需解析请求参数
-                    if (MapUtils.isEmpty(paramMap)) {
-                        sendMessage(eb, path, null, rc);
-                        return;
-                    }
-                    Map<String, String> params = new HashMap<>();
-                    // 获取GET请求参数
-                    for (String param : paramMap.keySet()) {
-                        params.put(param, rc.request().getParam(param));
-                    }
-                    HttpRequest httpRequest = HttpRequest.builder()
-                            .params(params)
-                            .body(null)
-                            .build();
-                    sendMessage(eb, path, httpRequest, rc);
-                });
+                .handler(rc ->
+                    processRoute(pathInfo, controllerInfo, rc, RequestMethod.GET)
+                );
     }
 
     /**
      * 路由POST请求
      */
     public void routePostReq(PathInfo pathInfo, ControllerInfo controllerInfo) {
-        EventBus eb = vertx.eventBus();
-        String path = pathInfo.getHttpMethod() + pathInfo.getHttpPath();
         router.post(pathInfo.getHttpPath())
                 // 为获取post请求的body，必须要加
                 .handler(BodyHandler.create())
-                .handler(rc -> {
-                    Map<String, Class<?>> paramMap = controllerInfo.getMethodParameter();
-                    // 入参为空，则无需解析请求参数
-                    if (MapUtils.isEmpty(paramMap)) {
-                        sendMessage(eb, path, null, rc);
-                        return;
-                    }
-                    Map<String, String> params = new HashMap<>();
-                    // 获取POST请求参数
-                    for (String param : paramMap.keySet()) {
-                        Optional.ofNullable(rc.request().getFormAttribute(param))
-                                .ifPresent(it -> params.put(param, it));
-                    }
-                    String body = null;
-                    // 获取body
-                    if (MapUtils.isEmpty(params) && paramMap.size() == 1) {
-                        body = rc.getBodyAsString();
-                    }
-                    HttpRequest httpRequest = HttpRequest.builder()
-                            .params(params)
-                            .body(body)
-                            .build();
-                    sendMessage(eb, path, httpRequest, rc);
-                });
+                .handler(rc ->
+                        processRoute(pathInfo, controllerInfo, rc, RequestMethod.POST)
+                );
     }
 
     /**
@@ -114,6 +76,45 @@ public class EventLoopVerticle extends AbstractVerticle {
         eb.send(path, msg, res ->
                 rc.response().putHeader("Content-type", "text/plain;charset=UTF-8")
                         .end(res.result().body().toString()));
+    }
+
+    /**
+     * 处理路由
+     */
+    private void processRoute(PathInfo pathInfo, ControllerInfo controllerInfo,
+                       RoutingContext rc, RequestMethod requestMethod) {
+        EventBus eb = vertx.eventBus();
+        // event bus传递消息的路径
+        String path = pathInfo.getHttpMethod() + pathInfo.getHttpPath();
+        Map<String, Class<?>> paramMap = controllerInfo.getMethodParameter();
+        // 入参为空，则无需解析请求参数
+        if (MapUtils.isEmpty(paramMap)) {
+            sendMessage(eb, path, null, rc);
+            return;
+        }
+        final Map<String, String> params = new HashMap<>();
+        String body = null;
+        // 获取请求参数
+        if (RequestMethod.POST.equals(requestMethod)) {
+            for (String param : paramMap.keySet()) {
+                Optional.ofNullable(rc.request().getFormAttribute(param))
+                        .ifPresent(it -> params.put(param, it));
+            }
+            // 获取body
+            if (MapUtils.isEmpty(params) && paramMap.size() == 1) {
+                body = rc.getBodyAsString();
+            }
+        } else if (RequestMethod.GET.equals(requestMethod)){
+            for (String param : paramMap.keySet()) {
+                Optional.ofNullable(rc.request().getParam(param))
+                        .ifPresent(it -> params.put(param, it));
+            }
+        }
+        HttpRequest httpRequest = HttpRequest.builder()
+                .params(params)
+                .body(body)
+                .build();
+        sendMessage(eb, path, httpRequest, rc);
     }
 
 }
